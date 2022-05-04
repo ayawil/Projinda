a11ae0f46630be681f0a66c415c26489eaa3a987
Fredrik Blomqvist <frblo@kth.se>
2022-05-04 11:02:50 +0200

    implemented basic class

--- src/Picture.cs
+++ src/Picture.cs
@@ -1,4 +1,28 @@
 class Picture
 {
+    private bitmap image;
 
+    public void create(string location)
+    {
+        try 
+        {
+            image = new bitmap(location);
+        }
+        catch(Exception)
+        {
+            Console.Writeline("There was a problem saving the file.");
+        }
+    }
+
+    public void export()
+    {
+        image.Save("export.png");
+    }
+
+    public int height()
+        { return image.height; }
+    public int lenght()
+        { return image.lenght; }
+    
+    
 }
