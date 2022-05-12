1e1638dc3a67d5e5d5a10a362cf6350d0181e5ec
Fredrik Blomqvist <frblo@kth.se>
2022-05-12 23:15:44 +0200

    function added but not working

--- src/Program.java
+++ src/Program.java
@@ -4,11 +4,20 @@
 
         // System.out.println("Image processor started correctly, please ");
 
-        String location = "Skog.jpeg";
-        image.create(location);
+        Filter filter;
+        switch (args[0]) {
+            case "pixel":
+                filter = new Pixelart(image);
+                break;
+            default:
+                System.out.println("Incorrect arguments");
+                return;
+        }
 
-        Filter pixelArt = new Pixelart(image);
-        pixelArt.addFilter();
+        String location = args[1];
+        image.create(location);
+        
+        filter.addFilter();
         image.export();
     }
 }
