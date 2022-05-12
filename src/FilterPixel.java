a995aace1afb2ea16097310181829d69d67c2ddf
Aya Wilhelmson <ayaw@kth.se>
2022-05-12 09:11:17 +0200

    Add average color method

--- src/FilterPixel.java
+++ src/FilterPixel.java
@@ -1,41 +1,47 @@
+import java.awt.Color;
+
 public class FilterPixel {
     private Picture image;
     private int pixelSize;
 
-    public FilterPixel(Picture image)
-    {
+    public FilterPixel(Picture image) {
         this.image = image;
         pixelSize = 4;
 
     }
 
-    public void addFilter()
-    {
-        for (int x = 0; x < image.lenght(); x += pixelSize)
-        {
-            for (int y = 0; y < image.height(); y += pixelSize)
-            {
+    public void addFilter() {
+        for (int x = 0; x < image.lenght(); x += pixelSize) {
+            for (int y = 0; y < image.height(); y += pixelSize) {
                 Picture imagePart = getPart(image, pixelSize, pixelSize, x, y);
                 int color = average(imagePart);
-                for (int i = 0; i < pixelSize; i++)
-                {
-                    for (int j = 0; j < pixelSize; j++)
-                    {
+                for (int i = 0; i < pixelSize; i++) {
+                    for (int j = 0; j < pixelSize; j++) {
                         image.setPixel(x + i, y + j, color);
                     }
                 }
-
             }
         }
     }
 
-    private Picture getPart(Picture image, int width, int height, int x, int y){
+    private Picture getPart(Picture image, int width, int height, int x, int y) {
 
-        return image;
+        return image.getSubimage(x, y, width, height);
     }
 
-    private int average(Picture image)
-    {
-        return 1;
+    private int average(Picture image) {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        for (int x = 0; x < image.lenght(); x++) {
+            for (int y = 0; y < image.height(); y++) {
+                Color currentColor = new Color(image.getPixel(x, y));
+                r += currentColor.getRed();
+                g += currentColor.getGreen();
+                b += currentColor.getBlue();
+            }
+        }
+        Color averageColor = new Color(r, g, b);
+        return averageColor.getRGB();
     }
 }
--- src/Picture.java
+++ src/Picture.java
@@ -4,27 +4,33 @@ public class Picture {
 
     private BufferedImage Image;
 
-    public void create(String location)
-    {
-            //Fix intilializatiom
-                Image = new BufferedImage(200, 200, 0);
+    public void create(String location) {
+        // Fix intilializatiom
+        Image = new BufferedImage(200, 200, 0);
 
-        
     }
 
-    public void export()
-    {
-       // Image.Save("export.png");
+    public void export() {
+        // Image.Save("export.png");
     }
 
-    public int height()
-    { return Image.getHeight(); }
-    public int lenght()
-    { return Image.getWidth(); }
+    public int height() {
+        return Image.getHeight();
+    }
 
-    public int getPixel(int x, int y)
-    { return Image.getRGB(x, y); }
+    public int lenght() {
+        return Image.getWidth();
+    }
 
-    public void setPixel(int x, int y, int color)
-    { Image.setRGB(x, y, color); }
+    public int getPixel(int x, int y) {
+        return Image.getRGB(x, y);
+    }
+
+    public void setPixel(int x, int y, int color) {
+        Image.setRGB(x, y, color);
+    }
+
+    public Picture getSubimage(int x, int y, int pixelSize) {
+        Image.getSubimage(x, y, pixelSize, pixelSize);
+    }
 }
