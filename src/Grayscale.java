c5bfc86e3effde630f42b619195d540a893cbcd2
Fredrik Blomqvist <frblo@kth.se>
2022-05-14 12:38:12 +0200

    Grayscale by Luminosity method implemented, close #37

--- src/Grayscale.java
+++ src/Grayscale.java
@@ -1,12 +1,32 @@
+import java.awt.Color;
+
 public class Grayscale extends Filter{
 
+    /**
+     * The constructor for the grayscale filter
+     * @param image the image of to be put into grayscale
+     */
     public Grayscale(Picture image) {
         super(image);
     }
 
     public void addFilter() {
-        // TODO Auto-generated method stub
+        for (int x = 0; x < image.width(); x++) {
+            for (int y = 0; y < image.height(); y++) {
+                image.setPixel(x, y, getGrayscale(image.getPixel(x, y)));
+            }
+        }
         
+    }
+
+    private int getGrayscale(int pixel) {
+        Color color = new Color(pixel);
+        int averageRBG = (color.getRed() + color.getGreen() + color.getBlue()) / 3;
+        int r = (int) (0.3 * averageRBG);
+        int g = (int) (0.59 * averageRBG);
+        int b = (int) (0.11 * averageRBG);
+
+        return new Color(r, g, b).getRGB();
     }
     
 }
